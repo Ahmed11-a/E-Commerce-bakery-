ecc65ebf7b1b3da748279361c2642a9242ae505c
Ahmed Mahmoud <75645860+Ahmed11-a@users.noreply.github.com>
2024-03-12 18:00:15 +0200

    Add files via upload

---
+++ E-Commerce-MVC/Program.cs
@@ -0,0 +1,67 @@
+using E_Commerce_MVC.Interfaces;
+using E_Commerce_MVC.Models;
+using E_Commerce_MVC.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace E_Commerce_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation(); ;
+
+            // IoC container Registerations
+            builder.Services.AddDbContext<ECommerceDB>(options => 
+                options.UseLazyLoadingProxies()
+                .UseSqlServer(builder.Configuration.GetConnectionString("remote"))
+            );
+
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredLength = 5;
+
+            }).AddEntityFrameworkStores<ECommerceDB>();
+
+            builder.Services.AddScoped<IProductRepo,ProductRepo>();
+            builder.Services.AddScoped<ICartItemRepo,CartItemRepo>();
+            builder.Services.AddScoped<ICategoryRepo,CategoryRepo>();
+            builder.Services.AddScoped<ICustomerRepo,CustomerRepo>();
+            builder.Services.AddScoped<IOrderItemRepo,OrderItemRepo>();
+            builder.Services.AddScoped<IPaymentRepo,PaymentRepo>();
+            builder.Services.AddScoped<IShipmentRepo,ShipmentRepo>();
+            builder.Services.AddScoped<IOrderRepo,OrderRepo>();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ E-Commerce-MVC/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const inputField = document.getElementById("c_code");
+const applyButton = document.getElementById("button-addon2");
+
+console.log(inputField);
+
+// Initially disable the button
+applyButton.disabled = true;
+
+// Event listener to handle input changes
+inputField.addEventListener("input", () => {
+	if (inputField.value.trim() !== "") {
+		applyButton.disabled = false;
+	} else {
+		applyButton.disabled = true;
+	}
+});
+
